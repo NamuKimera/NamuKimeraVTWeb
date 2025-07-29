e36c62c9587e44a8a6f25175c40d255fde4bf20b
NamuKimeraVT <namuquimera@gmail.com>
2025-07-29 01:31:13 -0300

    Creacion Galeria

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 
 const contenido = document.getElementById("content");
 
+function openModal(src) {
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("img01");
+  modal.style.display = "block";
+  modalImg.src = src;
+}
+
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
 alert("NamuKimeraVT © 2025 | Todos los derechos reservados");
